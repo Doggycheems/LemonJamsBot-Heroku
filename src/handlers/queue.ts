b0ad5afa3282e6aabf3f94a963c18b7642a55474
doggy <75123663+Doggycheems@users.noreply.github.com>
2021-02-16 17:23:45 +0100

    Update queue.ts

--- src/handlers/queue.ts
+++ src/handlers/queue.ts
@@ -1,7 +1,7 @@
 import { Composer } from 'telegraf';
 import { getQueue } from '../tgcalls';
 
-export const queueHandler = Composer.command('queue', ctx => {
+export const queueHandler = Composer.command('coda', ctx => {
     const { chat } = ctx.message;
 
     if (chat.type !== 'supergroup') {
@@ -12,7 +12,7 @@ export const queueHandler = Composer.command('queue', ctx => {
     const message =
         queue && queue.length > 0
             ? queue.map((url, index) => `${index + 1}. ${url}`).join('\n')
-            : 'The queue is empty.';
+            : 'La coda è vuota :/.';
 
     ctx.reply(message);
 });
