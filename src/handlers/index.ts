d8d27ea4390b19d9528d3d0a59721b22a6ed8e4a
doggy <75123663+Doggycheems@users.noreply.github.com>
2021-02-16 17:34:13 +0100

    Update index.ts

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -4,10 +4,14 @@ import { playHandler } from './play';
 import { queueHandler } from './queue';
 import { pauseHandler } from './pause';
 import { skipHandler } from './skip';
+import { startHandler } from './start';
+import { helpHandler } from './help';
 
 export const initHandlers = (): void => {
     bot.use(playHandler);
     bot.use(queueHandler);
     bot.use(pauseHandler);
     bot.use(skipHandler);
+    bot.use(startHandler);
+    bot.use(helpHandler);
 };
