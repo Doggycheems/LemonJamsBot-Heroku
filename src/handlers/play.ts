44563c0733a956ea1e4963ff04494bdef9d3352f
doggy <75123663+Doggycheems@users.noreply.github.com>
2021-02-16 17:22:35 +0100

    Update play.ts

--- src/handlers/play.ts
+++ src/handlers/play.ts
@@ -5,7 +5,7 @@ export const playHandler = Composer.command('play', async ctx => {
     const { chat } = ctx.message;
 
     if (chat.type !== 'supergroup') {
-        await ctx.reply('I can only play in groups.');
+        await ctx.reply('Mi dispiace ma posso funzionare solo nei supergruppi.');
         return;
     }
 
@@ -13,11 +13,11 @@ export const playHandler = Composer.command('play', async ctx => {
     const text = ctx.message.text.slice(commandEntity.length + 1);
 
     if (!text) {
-        await ctx.reply('You need to specify a YouTube URL.');
+        await ctx.reply('Coglione devi darmi un link youtube.');
         return;
     }
 
     const index = await addToQueue(chat, text);
 
-    await ctx.reply(index === 0 ? 'Playing.' : `Queued at position ${index}.`);
+    await ctx.reply(index === 0 ? 'Avvio la canzone...' : `Queued at position ${index}.`);
 });
